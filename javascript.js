1b63b7d271bfb5ff693c0d6fceb7ab25624a3474
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2025-02-20 15:27:25 +1300

    Created logic for playing a round

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 console.log("Hello World");
 
+let playerScore = 0;
+let computerScore = 0;
 
 /*psuedocode logic 
 The player will be prompted to play rock, paper or scissors.
@@ -7,7 +9,7 @@ The computer will randomly play rock, paper or scissors
 The winner will be displayed*/
 
 
-function computerChoice(){
+function getComputerChoice(){
     //Rock will be 1, paper will be 2, scissors will be 3.
 
     let choice = Math.floor(Math.random()*3 + 1); //+1 for 1-3 instead of 0-2
@@ -18,4 +20,34 @@ function computerChoice(){
 
 }
 
-console.log(computerChoice());
+function getPlayerChoice(){
+    let hand  = prompt("Rock, Paper, Scissors? ", "Rock");
+    return hand;
+}
+
+
+function updateScore(){
+    
+}
+
+
+function playRound(){
+    let compHand = getComputerChoice();
+    let playerHand = getPlayerChoice().toLowerCase();
+    playerHand = playerHand[0].toUpperCase() + playerHand.slice(1);
+    if (compHand == playerHand){
+        return "It's a tie!";
+    }
+    else if ((compHand == "Rock" && playerHand == "Scissors") || (compHand == "Paper" && playerHand == "Rock") || (compHand == "Scissors" && playerHand == "Paper")){
+        computerScore += 1;
+        return `Computer wins! ${compHand} beats ${playerHand}`;
+    }
+    else{
+        return `Player wins! ${playerHand} beats ${compHand}`;
+    }
+
+}
+
+
+
+console.log(playRound());

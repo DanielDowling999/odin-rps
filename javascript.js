163f1ce85302dee1af38026bf62a9527eae2dcf8
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2025-05-01 19:27:33 +1200

    Completed gui

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
 console.log("Hello World");
 
-
-
-/*psuedocode logic 
-The player will be prompted to play rock, paper or scissors.
-The computer will randomly play rock, paper or scissors
-The winner will be displayed*/
-
-
 function getComputerChoice(){
     //Rock will be 1, paper will be 2, scissors will be 3.
 
@@ -19,49 +11,77 @@ function getComputerChoice(){
 
 }
 
-function getPlayerChoice(){
-    let hand  = prompt("Rock, Paper, Scissors? ", "Rock");
-    return hand;
-}
-
 
 
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
-    function playRound(){
+
+    const gameButtons = document.querySelectorAll(".rps");
+    const result = document.querySelector("#result-msg");
+    const cScore = document.querySelector("#cScore");
+    const pScore = document.querySelector("#pScore");
+    const restart = document.querySelector(".restart")
+    const victory = document.querySelector("#victory")
+    const msg = document.querySelector("#msg");
+    const played = document.querySelector("#player .last-played");
+    const compPlayed = document.querySelector("#computer .last-played");
+    gameButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            playRound(button.id);
+        });
+    });
+
+    restart.addEventListener('click', () => {
+        restartGame();
+    });
+
+    function playRound(playerHand){
         let compHand = getComputerChoice();
-        let playerHand = getPlayerChoice().toLowerCase();
-        playerHand = playerHand[0].toUpperCase() + playerHand.slice(1);
+        played.textContent = playerHand == "Rock" ? "🪨" : playerHand == "Paper" ? "📃" : "✂️";  
+        compPlayed.textContent = compHand == "Rock" ? "🪨" : compHand == "Paper" ? "📃" : "✂️";
         if (compHand == playerHand){
-            console.log("It's a tie!");
+            result.textContent = "It's a Tie!";
+            console.log("It's a Tie!");
         }
         else if ((compHand == "Rock" && playerHand == "Scissors") || (compHand == "Paper" && playerHand == "Rock") || (compHand == "Scissors" && playerHand == "Paper")){
             computerScore += 1;
+            result.textContent = `Computer wins! ${compHand} beats ${playerHand}!`;
+            cScore.textContent = `${computerScore}`;
             console.log(`Computer wins! ${compHand} beats ${playerHand}`);
         }
         else{
             playerScore += 1;
+            result.textContent = `Player wins! ${playerHand} beats ${compHand}!`;
+            pScore.textContent = `${playerScore}`;
             console.log(`Player wins! ${playerHand} beats ${compHand}`);
         }
-    
+    function gameOver(winner){
+        msg.textContent = `The ${winner} wins!`;
+        victory.style.visibility = 'visible';
     }
 
-    for(let i = 0; i< 5; i++){
-        playRound();
-        console.log(`Player Points: ${playerScore}, Computer Points: ${computerScore}`);
+    if (playerScore >= 5){
+        gameOver("Player");
     }
-    if (playerScore > computerScore){
-        console.log("Player wins!");
+    else if (computerScore >= 5){
+        gameOver("Computer");
     }
-    else if (playerScore < computerScore){
-        console.log("Computer wins!");
+    
     }
-    else{
-        console.log("It's a tie...");
+    function restartGame(){
+        playerScore = 0;
+        computerScore = 0;
+        pScore.textContent = 0;
+        cScore.textContent = 0;
+        result.textContent = "Pick a hand!";
+        victory.style.visibility = 'hidden';
     }
 }
 
-playGame();
+
+
+
+document.addEventListener("DOMContentLoaded", playGame);
 
 
